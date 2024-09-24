94db0fe0ea735c48a6f05170d06c766cc1912efd
Neto Russo <netoacrn1@gmail.com>
2024-09-24 16:35:45 -0300

    App

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import Banner from "@/components/Banner";
+import Menu from "@/components/Menu";
+
+export default function Home() {
+  return (
+    <>
+    <header>
+      <Menu />
+      <Banner />
+    </header>
+    <main>
+
+    </main>
+    </>
+  );
+}
