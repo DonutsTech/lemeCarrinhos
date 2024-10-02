26df47b553ac4772347e08d94c3272e18149de00
Neto Russo <netoacrn1@gmail.com>
2024-10-01 23:07:00 -0300

    adicionando novas seções

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,27 @@
 import About from "@/components/About";
 import Banner from "@/components/Banner";
 import Clientes from "@/components/Clientes";
+import Contatos from "@/components/Contatos";
 import Menu from "@/components/Menu";
 import Servicos from "@/components/Servicos";
 import Slider from "@/components/Slider";
 
 export default function Home() {
+
+
   return (
     <>
-    <header>
-      <Menu />
-      <Banner />
-    </header>
-    <main>
-      <Slider />
-      <About />
-      <Servicos />
+      <header>
+        <Menu />
+        <Banner />
+      </header>
+      <main>
+        <Slider />
+        <About />
+        <Servicos />
+      </main>
       <Clientes />
-    </main>
+      <Contatos />
     </>
   );
 }
