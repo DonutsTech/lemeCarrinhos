851dea25ea7da424f9b3a607d6aa4579ebd6517b
Milena Leme <milenaleme4@hotmail.com>
2025-02-27 16:39:29 -0300

    inserindo arrumação do slider

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -11,10 +11,10 @@ import s5 from './imagens/05_slide.jpg';
 import Image from 'next/image';
 import { Autoplay, EffectCoverflow, Navigation, Pagination } from "swiper/modules";
 
-const Slider = () => { 
+const Slider = () => {
   return (
     <section aria-label='Slide com fotos da Leme Carrinhos' className={Style.slider}>
-        <h2>Nossa equipe possui vasta experiência e conhecimento no segmento de carrinhos de supermercado, o que nos permite oferecer soluções personalizadas para cada cliente.  
+        <h2>Nossa equipe possui vasta experiência e conhecimento no segmento de carrinhos de supermercado, o que nos permite oferecer soluções personalizadas para cada cliente.
         </h2>
       <div className={Style.sliderContent}>
         <Swiper
@@ -43,19 +43,19 @@ const Slider = () => {
           id='slides'
         >
           <SwiperSlide>
-            <Image className={Style['img-slider']} src={s1} alt='Uma mulher está fazendo compras em um supermercado com um homem. Ela tem uma cesta cheia de frutas e legumes, incluindo cenoura e brócolis. O homem está segurando a cesta enquanto ela olha seus itens. Eles estão cercados por outras pessoas na loja, algumas das quais também carregam sacolas ou carrinhos de compras.'  />
+            <Image className={Style['img-slider']} src={s1} alt='Uma mulher está fazendo compras em um supermercado com um homem. Ela tem uma cesta cheia de frutas e legumes, incluindo cenoura e brócolis. O homem está segurando a cesta enquanto ela olha seus itens. Eles estão cercados por outras pessoas na loja, algumas das quais também carregam sacolas ou carrinhos de compras.' priority />
           </SwiperSlide>
           <SwiperSlide>
-            <Image className={Style['img-slider']} src={s2} alt='Num supermercado bem iluminado, um homem e uma mulher fazem compras juntos. O homem veste uma camisa branca com as mangas arregaçadas até os cotovelos, enquanto a mulher veste uma jaqueta jeans azul por cima de um top de cor clara. Ambos têm carrinhos cheios de mantimentos, incluindo produtos frescos como cenoura e brócolis. Seus sorrisos sugerem que estão gostando da experiência de compra.'  />
+            <Image className={Style['img-slider']} src={s2} alt='Num supermercado bem iluminado, um homem e uma mulher fazem compras juntos. O homem veste uma camisa branca com as mangas arregaçadas até os cotovelos, enquanto a mulher veste uma jaqueta jeans azul por cima de um top de cor clara. Ambos têm carrinhos cheios de mantimentos, incluindo produtos frescos como cenoura e brócolis. Seus sorrisos sugerem que estão gostando da experiência de compra.' priority />
           </SwiperSlide>
           <SwiperSlide>
-            <Image className={Style['img-slider']} src={s3} alt='Nesta cena vibrante, um homem e uma mulher estão atrás de uma mesa repleta de produtos frescos num mercado ao ar livre. O homem usa chapéu de palha enquanto a mulher está adornada com uma blusa amarela. Eles estão rodeados por uma variedade de frutas e vegetais, incluindo maçãs, laranjas e bananas, que dão um toque de cor ao cenário. Ao fundo, outros compradores podem ser vistos percorrendo as bancas do mercado, aumentando a atmosfera animada. A imagem captura a essência de um dia de mercado movimentado, repleto de produtos frescos e interações animadas.'  />
+            <Image className={Style['img-slider']} src={s3} alt='Nesta cena vibrante, um homem e uma mulher estão atrás de uma mesa repleta de produtos frescos num mercado ao ar livre. O homem usa chapéu de palha enquanto a mulher está adornada com uma blusa amarela. Eles estão rodeados por uma variedade de frutas e vegetais, incluindo maçãs, laranjas e bananas, que dão um toque de cor ao cenário. Ao fundo, outros compradores podem ser vistos percorrendo as bancas do mercado, aumentando a atmosfera animada. A imagem captura a essência de um dia de mercado movimentado, repleto de produtos frescos e interações animadas.' priority />
           </SwiperSlide>
           <SwiperSlide>
-            <Image className={Style['img-slider']} src={s4} alt='Uma mulher está sentada em um carrinho de compras, sorrindo e relaxada. Ela está com os pés apoiados na alça do carrinho de compras, aproveitando o tempo na loja.' />
+            <Image className={Style['img-slider']} src={s4} alt='Uma mulher está sentada em um carrinho de compras, sorrindo e relaxada. Ela está com os pés apoiados na alça do carrinho de compras, aproveitando o tempo na loja.' priority />
           </SwiperSlide>
           <SwiperSlide>
-            <Image className={Style['img-slider']} src={s5} alt='Um jovem empurra um carrinho com uma grande caixa de papelão por uma rua residencial. Ele está vestindo uma camiseta preta e jeans, e a caixa parece ter algum tipo de logotipo ou desenho. O cenário sugere que pode ser final de tarde, dada a iluminação suave e as sombras das casas.' />
+            <Image className={Style['img-slider']} src={s5} alt='Um jovem empurra um carrinho com uma grande caixa de papelão por uma rua residencial. Ele está vestindo uma camiseta preta e jeans, e a caixa parece ter algum tipo de logotipo ou desenho. O cenário sugere que pode ser final de tarde, dada a iluminação suave e as sombras das casas.' priority />
           </SwiperSlide>
         </Swiper>
       </div>
