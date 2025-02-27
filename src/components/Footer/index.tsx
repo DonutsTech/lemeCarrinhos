873f2c8dc8aaa8bf97735229483ad9180cf8d734
Milena Leme <milenaleme4@hotmail.com>
2025-02-27 16:42:38 -0300

    modificação do componente footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -11,8 +11,8 @@ const Footer = () => {
   return (
     <footer className={Style.footer}>
       <div className={Style.copy}>
-      <a href='/' className={Style.linkLogo}> 
-        <Image src={logo} alt="logo da Leme" className={Style.logo} />
+      <a href='/' className={Style.linkLogo}>
+        <Image src={logo} alt="logo da Leme" className={Style.logo} priority/>
       </a>
       <p className={Style.footerText}>
         Todos os direitos reservados &copy; Leme Manutencao e Reformas LTDA || 19.055.182/0001-26 || 2024
@@ -24,7 +24,7 @@ const Footer = () => {
           Feito Com muito amor e açucar
         </p>
         <a href='https://www.donutstech.com.br/' target='_blank' rel='noreferrer'>
-          <Image src={donuts} alt="donuts" className={Style.donutsImg} />
+          <Image src={donuts} alt="donuts" className={Style.donutsImg} priority/>
         </a>
       </div>
     </footer>
