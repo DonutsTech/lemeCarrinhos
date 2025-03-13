21c247bfcbe5f72f2ba8441b5a88cfe05ad5fe51
Neto Russo <netoacrn1@gmail.com>
2025-03-13 16:59:22 -0300

    Adicionando o SEO e alterando o Favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,10 +4,29 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Leme Carrinhos",
   description: "Leme carrinhos, Carrinhos de mercado e serviço que não te deixam na mão",
-  authors: [{name: "Donuts Tech"}],
+  authors: [{ name: "Donuts Tech" }],
+  keywords: ["carrinhos de mercado", "carrinhos de serviço", "durabilidade", "qualidade", "Leme Carrinhos"],
+  openGraph: {
+    title: "Leme Carrinhos - Carrinhos para mercado e serviços de alta qualidade",
+    description: "Os melhores carrinhos de mercado e serviços que garantem durabilidade e eficiência.",
+    url: "https://www.lemecarrinhos.com.br",
+    siteName: "Leme Carrinhos",
+    images: [
+      {
+        url: "https://www.lemecarrinhos.com.br/assets/imgOPG.jpg", // Substitua por uma imagem real
+        width: 1200,
+        height: 630,
+        alt: "Leme Carrinhos - Qualidade e durabilidade",
+      },
+    ],
+    locale: "pt_BR",
+    type: "website",
+  },
+  robots: "index, follow",
+  alternates: {
+    canonical: "https://www.lemecarrinhos.com.br",
+  },
 };
-
-
 
 
 export default function RootLayout({
